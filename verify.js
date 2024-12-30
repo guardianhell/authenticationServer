f807863cbfd983f6a9c5cc0e7cb1a34f01db1906
Calvin <calvinsidik@gmail.com>
2024-12-31 00:39:58 +0700

    auth verify

--- verify.js
+++ verify.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async function (req, res, next) {
   try {
-    var token = req.cookie;
+    var token = req.cookies.Authorization;
 
     console.log(token);
 
